bb216912e35c84443b375566fdd692b332aefb4f
enginergagal <enginergagal@gmail.com>
2023-04-14 10:12:39 +0700

    update 4

--- app/Views/html_to_pdf.php
+++ app/Views/html_to_pdf.php
@@ -1,44 +1,52 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>File pdf</title>
+
+    <style>
+     /* Apply minimalistic styling to the table */
+     table {
+       border-collapse: collapse;
+       width: 100%;
+     }
+
+     th, td {
+       text-align: left;
+       padding: 8px;
+       border-bottom: 1px solid #ddd;
+     }
+
+     th {
+       background-color: #f2f2f2;
+     }
+   </style>
   </head>
   <body>
-    <h1>Hello, world!</h1>
 
 
-    <table class="table">
+<table>
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th>No</th>
+      <th>Nama</th>
+      <th>Nim</th>
+      <th>Jabatan</th>
+      <th>NO Telp</th>
+      <th>Pesan</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>1</td>
+      <td>Gilbert Lua</td>
+      <td>612017050</td>
+      <td>Ketua SMF</td>
+      <td>085226510326</td>
+      <td>Jangan pernah berubah untuk terus menjadi pribadi yang lebih menyayangi tuhan</td>
+
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+    </tbody>
 </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
 </html>
