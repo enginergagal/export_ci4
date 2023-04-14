866fdbe3a7e374a2980603063c228199cd5affce
enginergagal <enginergagal@gmail.com>
2023-04-14 09:48:31 +0700

    update 2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,14 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    public function fpdf(){
+      $mpdf = new \Mpdf\Mpdf();
+  		$html = view('html_to_pdf',[]);
+  		$mpdf->WriteHTML($html);
+  		$this->response->setHeader('Content-Type', 'application/pdf');
+  		$mpdf->Output('output.pdf','I'); // opens in browser
+      //return view('welcome_message');
+  		//$mpdf->Output('arjun.pdf','D'); // it downloads the file into the user system, with give name
+
+    }
 }
